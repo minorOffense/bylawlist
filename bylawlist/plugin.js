cb1b47e9899797854b3f057735f177e0c400f053
Melissa Roy <mroy@coldfrontlabs.ca>
2017-12-22 14:13:18 -0500

    fix(js): removing capital L's and adjust label

--- bylawlist/plugin.js
+++ bylawlist/plugin.js
@@ -1,15 +1,15 @@
 CKEDITOR.plugins.add( 'bylawlist', {
     icons: 'bylawlist', // Bylaw List icon
     init: function( editor ) {
-        editor.addCommand( 'insertBylawList', {
+        editor.addCommand( 'insertBylawlist', {
             exec: function( editor ) {
                 var now = new Date();
                 editor.insertHtml( 'The current date and time is: <em>' + now.toString() + '</em>' );
             }
         });
         editor.ui.addButton( 'Bylawlist', {
-            label: 'Bylaw List',
-            command: 'insertBylawList',
+            label: 'Add Bylawlist',
+            command: 'insertBylawlist',
             toolbar: 'insert'
         });
     }
