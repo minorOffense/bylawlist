82b11e1f3c99511c75c86268fa401d2fb33bf81f
Melissa Roy <mroy@coldfrontlabs.ca>
2018-01-03 17:25:36 +0000

    fix(js): target single ol

--- plugin.js
+++ plugin.js
@@ -3,14 +3,29 @@ CKEDITOR.plugins.add( 'bylawlist', {
     init: function( editor ) {
         editor.addCommand( 'insertBylawlist', {
             exec: function( editor ) {
-                var now = new Date();
-                editor.insertHtml( 'The current date and time is: <em>' + now.toString() + '</em>' );
+              if ('li' == editor.getSelection().getStartElement().getName()) {  
+                if ('OL' == editor.getSelection().getStartElement().$.parentNode.nodeName) {
+                  var bylaw = editor.getSelection().getStartElement().$.parentNode;
+                  if(bylaw.className.indexOf("bylawlist") < 0){
+                     bylaw.className += "bylawlist";
+                  } 
+                  else {
+                    bylaw.className = "";
+                  }
+                  
+                }              
+              }
             }
-        });
+       });
         editor.ui.addButton( 'Bylawlist', {
             label: 'Add Bylawlist',
             command: 'insertBylawlist',
             toolbar: 'insert'
         });
+        var format = {'element': 'Bylawlist'};
+        var style = new CKEDITOR.style(format);
+        editor.attachStyleStateChange( style, function( state ) {
+   	      !editor.readOnly && editor.getCommand( 'insertBylawlist' ).setState( state );
+        } );
     }
 });
