30714c6d954c7adcec1737446d2f4662333be3be
Mathew Winstone <mwinstone@coldfrontlabs.ca>
2019-07-09 15:42:17 -0400

    fix(acf): insert acf into the right place
    Can't use short format
    
    Add specific class it's allowed to use

--- plugin.js
+++ plugin.js
@@ -1,6 +1,4 @@
 CKEDITOR.plugins.add( 'bylawlist', {
-  allowedContent: 'ol[class,start] ul[class]',
-  requiredContent: 'ol[class,start] ul[class]',
   onLoad: function() {
     CKEDITOR.addCss(
       'ol.bylawlist{list-style-type:none;list-style-position:inside;padding-left:1.5em}ol.bylawlist li{counter-increment:bylawlist-counter !important}ol.bylawlist li:first-child{counter-reset:bylawlist-counter}ol.bylawlist>li:before{content:"(" counter(bylawlist-counter,decimal) ") " !important}ol.bylawlist>li>ol{list-style-type:inherit}ol.bylawlist>li>ol>li:before{content:"(" counter(bylawlist-counter,lower-alpha) ") "}ol.bylawlist>li>ol>li>ol{list-style-type:inherit}ol.bylawlist>li>ol>li>ol>li:before{content:"(" counter(bylawlist-counter,lower-roman) ") "}ol.bylawlist>li>ol>li>ol>li>ol{list-style-type:inherit}ol.bylawlist>li>ol>li>ol>li>ol>li:before{content:"(" counter(bylawlist-counter,decimal) ") "}ol.bylawlist>li ol>li:before{content:"(" counter(bylawlist-counter,lower-alpha) ") " !important}ol.bylawlist>li ol>li ol>li:before{content:"(" counter(bylawlist-counter,lower-roman) ") " !important}ol.bylawlist>li ol>li ol>li ol>li:before{content:counter(bylawlist-counter,decimal) ". " !important}'
@@ -9,6 +7,12 @@ CKEDITOR.plugins.add( 'bylawlist', {
   icons: 'bylawlist', // Bylaw List icon
   init: function( editor ) {
       editor.addCommand( 'insertBylawlist', {
+        allowedContent: {
+          'ol': {
+            attributes: ['class', 'start'],
+            classes: 'bylawlist'
+          },
+        },
           exec: function( editor ) {
             if ('li' == editor.getSelection().getStartElement().getName()) {
               if ('OL' == editor.getSelection().getStartElement().$.parentNode.nodeName) {
@@ -26,7 +30,7 @@ CKEDITOR.plugins.add( 'bylawlist', {
     editor.ui.addButton( 'Bylawlist', {
         label: 'Add Bylawlist',
         command: 'insertBylawlist',
-        toolbar: 'insert'
+        toolbar: 'insert',
     });
     var format = {'element': 'Bylawlist'};
     var style = new CKEDITOR.style(format);
